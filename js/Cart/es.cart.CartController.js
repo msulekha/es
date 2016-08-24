b42b38a4725871c2866452ac050be5828ef004e7
msulekha <sulekhamahato@gmail.com>
2016-08-23 23:19:56 -0700

    CartController comments added.

--- js/Cart/es.cart.CartController.js
+++ js/Cart/es.cart.CartController.js
@@ -3,6 +3,12 @@
 
     module.controller('CartController', CartController);
 
+    /**!
+     * CartController: Controller function for the cart page
+     * @param $scope
+     * @param cartService
+     * @constructor
+     */
     function CartController($scope, cartService){
         $scope.cartItems = cartService.getCartItems();
         $scope.getTotalAmount = getTotalAmount;
@@ -11,10 +17,19 @@
         _init();
 
         ///////// method definitions ///////////
+        /**!
+         * _init: initial function
+         * @private
+         */
         function _init() {
             console.log('CartController ==> SCOPE ID :: ' + $scope.$id);
         }
 
+        /**!
+         * getTotalAmount: function to calculate the total 
+         *                  items added to the cart
+         * @returns {number}
+         */
         function getTotalAmount() {
             var total = 0.00;
             for(var i = 0; i < $scope.cartItems.length; i++) {
